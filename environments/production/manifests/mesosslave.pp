09f5150d628402fe5bdf5943341689628ecfc2f8
as0bu <towen27@gmail.com>
2016-01-26 00:22:24 -0700

    Add Mesosphere repo and listening address for slaves

--- environments/production/manifests/mesosslave.pp
+++ environments/production/manifests/mesosslave.pp
@@ -1,6 +1,10 @@
+class{'mesos':
+  repo => 'mesosphere',
+}
+
 class{'mesos::slave':
   zookeeper  => 'zk://192.168.1.1:2181,192.168.1.2:2181,192.168.1.3:2181/mesos',
-  listen_address => $::ipaddress,
+  listen_address => $::ipaddress_eth1,
   attributes => {
     'env' => 'production',
   },
