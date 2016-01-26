e29483812a890cf4b53c34ba791004b806453744
as0bu <towen27@gmail.com>
2016-01-26 01:18:19 -0700

    adding master ip to listen on

--- environments/production/manifests/mesosmaster.pp
+++ environments/production/manifests/mesosmaster.pp
@@ -3,9 +3,10 @@ class {'mesos':
 }
 
 class{'mesos::master':
-  zookeeper  => 'zk://192.168.11.11:2181,192.168.11.12:2181,192.168.11.13:2181/mesos',
-  work_dir   => '/var/lib/mesos',
-  options    => {
+  zookeeper      => 'zk://192.168.11.11:2181,192.168.11.12:2181,192.168.11.13:2181/mesos',
+  work_dir       => '/var/lib/mesos',
+  options        => {
     quorum   => 2
   },
+  listen_address => $::ipaddress_eth1
 }
